9b568a9c9750837305ca4f7ac6ef33cdf66e1296
PikachuBoss69 <anujnegi7367@gmail.com>
2026-07-18 14:06:27 +0000

    declaration merging for request parameter

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,15 @@
 import { IUserDocument } from "../models/users.model";
 
 declare global {
+
     namespace Express {
+
         interface Request {
+            
             user?: IUserDocument;
         }
     }
+
 }
 
 export {};
