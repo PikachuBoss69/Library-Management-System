d1cb3c648d87c720d80358f50926a79b0c3e8165
pikachuBOSS <anujnegi7367@gmail.com>
2026-08-24 22:25:00 +0530

    cors implemented

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,11 +7,19 @@ import borrowRoutes from "./routes/borrow.route";
 import bookCopyRoutes from './routes/bookCopy.route';
 import fineRoutes from './routes/fine.route';
 import  DashboardRoutes  from "./routes/dashboard.route";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 //Routes
 app.use("/api/auth", authRoutes);
